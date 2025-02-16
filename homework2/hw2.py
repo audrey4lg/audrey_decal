ab029beedc665d82646da38f57c4239dc25249ed
Audrey Lee-Gardner <audreylg@Audreys-MacBook-Air.local>
2025-02-16 00:12:59 -0800

    finished hw2

--- homework2/hw2.py
+++ homework2/hw2.py
@@ -1,10 +1,20 @@
 # 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# "cd" to get back to your home directory or you can do "cd ../" to get in directories that are the umbrella of the one you are currently in.
 # 2) What does ~/ mean?
+# this is an absolute path to get anywhere by saying its path to get there from home
 # 3) What's the difference between and abolute and a relative path in your terminal?
+# The absolute path lists the entire path that needs to be traveled from the home directory to where you are trying to go. While the relative path starts from wherever you are and uses the hierarchy and folders above you to create a shortcut.
 # 4) What command would bring you back to your home directory?
+# simply "cd"
 # 5) If you called rm ./ in your current working directory, what would it do? 
+# it would remove what you specify
 # 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# "Git add" essentially saves what you just worked on into the "staging area". "Git commit" saves it as a final draft to your local repository with a caption explaining what you did. "Git push" sends your final draft to the remote rpository and posting it on the internet for everyone to see.
 # 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+#  If you call "git add" and then "git status" it will tell you what you just added to the staging area and let you know that you need to still commit and push it.
 # 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# I have not had any major errors or bugs the weirdest thing was it seemed like my vs code somehow was uninstalled or I could not find it on my laptop but turns out I had just saved the application under a different name somehow.
 # 9) How does cloning a repository differ from pulling from a repository?
+# cloning a repository is more like just duplicating the entire folder that exists while pulling is like getting the new updates. Kind of like cloning is the initial following of someones instagram and seeing what they have posted and pulling is checking to see what they posted and whats going on in their life.
 # 10) Tell me a fun fact!
+# I once sewed my finger with a sewing machine so I had a piece of thread stuck in my finger under the skin for years. 
