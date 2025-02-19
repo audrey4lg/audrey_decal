7836618607f78807e521d5dc66f167d466a92505
Audrey Lee-Gardner <audreylg@Audreys-MacBook-Air.local>
2025-02-18 21:40:29 -0800

    finished hw1

---
+++ hw1.py
@@ -0,0 +1,12 @@
+#pwd is print working directory. This when you want to know where you are, it can be followed with a command of ls to find out everything in this folder you are currently in.
+#cd means change directory and is used when you want to be in a different folder. It is followed by the folder you would like to be in.
+#ls is to tell you everything in your current directory 
+#mkdir means to make a directory and is followed by what you want to name it
+#cp means to copy and it can be done to a file or a folder
+#mv is to move a file or folder into a different folder or directive and can be used when you want to move something.
+#1 Git is more like the version history of the coding world, while Github is more like the sharing and comment abilities of google docs but for code. Gitbash is Github but so it can be used on windows.
+#2 The terminal is the application in which commands can be run and a command line is the "sentence or command" that you write in terminal to tell it what to do.
+#3  Windows powershell is simply in a different language so windows need Gitbash in order to write in the same language Bash as macs.
+#4  Anaconda is more data science focused and includes Jupiter Notebook and uses the language of python. Python is simply a programming language and less of a software where other things can be done. Conda seems to be the manager/organizer of the anaconda program.
+#5 Jupyter Notebook is another platform that uses different languages but is used more for data science. Jupyter Lab seems to offer a large selection of things including notebooks.
+#6 VS Code is simply another software that uses the language of python to code.
